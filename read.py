4c173e39ac18c6d0be0ec9e6ab98a448674f1088
Ivy Wang <ivywty@gmail.com>
2021-05-06 13:20:42 +0800

    third try

--- read.py
+++ read.py
@@ -12,3 +12,10 @@ for  y in data:
 	sum_len += len(y)
 print('檔案讀取完了，共', len(data), '筆留言')
 print('每筆留言平均為', sum_len / len(data))
+
+new = []
+for y in data:
+	if len(y) < 100:
+		new.append(y)
+print('留言字數大於100的共有', len(data)-len(new), '筆留言')
+print('留言字數小於100的共有', len(new), '筆留言')
